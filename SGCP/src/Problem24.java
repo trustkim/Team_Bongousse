670a922565d7e4d406a1cbd77da1c47d580f8cee
trustkim <trustkim90@gmail.com>
2015-02-23 00:37:59 +0900

    리커전 모르겠드아...

--- SGCP/src/Problem24.java
+++ SGCP/src/Problem24.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.HashSet;
  
@@ -12,7 +13,7 @@ public class Problem24 {
 				int[] letters_in_a_line = new int[3];
 				String[] lines = new String[3];						// array for str1, str2, str3.
 				HashSet<Character> hs = new HashSet<Character>(10);	// hash set for counting arguments.
-				boolean result = true;
+
 				for(int i=0;i<3;i++){
 					letters_in_a_line[i] = input.nextInt();		// read number of letters in each lines.
 				}	// read letters at a line
@@ -23,20 +24,25 @@ public class Problem24 {
 						char temp = lines[i].charAt(j);	// read a letter in a line
 						if(!hs.contains(temp)){
 							hs.add(temp);
-							if(hs.size()>MAX_KEY_SIZE) {result = false; break;}	// ex. ABCDEFGHIJK
+							if(hs.size()>MAX_KEY_SIZE) break;	// ex. ABCDEFGHIJK
 						}
 					}
 				}
+
 				int key_size = hs.size(); char[] key_set = new char[MAX_KEY_SIZE];
 				if(key_size<=MAX_KEY_SIZE){
-					for(int i=0;i<MAX_KEY_SIZE;i++){
-						key_set[i] = hs.iterator().next();
+					Iterator<Character> iter=hs.iterator(); int index =0;
+					while(iter.hasNext()){
+						key_set[index++] = iter.next();
+					}
+					for(int i=key_size;i<MAX_KEY_SIZE;i++){
+						key_set[i] = '0';
 					}	// init key_set
-				}
-				char[] test_set = {'A'};
-				System.out.println(trans_str_to_int("AAA",test_set));
-				if(result) System.out.println("YES"); // print result
-				else System.out.println("NO");
+					
+					if(try_puzzle_rules(lines, key_set, key_size, 0)){
+						System.out.println("YES"); // print result
+					}else System.out.println("NO");
+				}else System.out.println("NO");
 			}			
 			input.close();
 		}catch(FileNotFoundException e){System.out.println("file not found..");}	
@@ -56,16 +62,24 @@ public class Problem24 {
 		default: System.exit(1); return '!';
 		}
 	}
-	public static int trans_str_to_int(String line, char[] key_set){
+	public static int trans_str_to_int(String line, char[] key_set, int key_size){
 		for(int i=0;i<key_set.length;i++){
+			if(i==key_size) break;
 			if(key_set[i]!='0')
-				line.replace(key_set[i], toChar(i));
+				line = line.replace(key_set[i], toChar(i));
 		}
 		return Integer.parseInt(line);
 	}
-	public static void try_puzzle_rules(String[] lines, char[] key_set){
-		// recursion
-		// resemble key_set...
-		// if(trans_str_to_int(str1, key_set)+trans_str_to_int(str2, key_set)==trans_str_to_int(str3, key_set)
+	public static boolean try_puzzle_rules(String[] lines, char[] key_set, int key_size, int index){
+//		// recursion
+//		if(trans_str_to_int(lines[0], key_set, key_size)+trans_str_to_int(lines[1], key_set, key_size)==trans_str_to_int(lines[2], key_set, key_size)){
+//			return true;
+//		}else {
+//			for(int i=index;i<MAX_KEY_SIZE;i++){
+//				// resemble key_set...
+//				return try_puzzle_rules(lines, key_set, key_size, i);
+//			}
+//		}
+		return false;
 	}
 }
