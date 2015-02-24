33515ace0fffcded5f454a50afddc3c5b6503354
TEST <TEST@TEST-PC>
2015-02-24 17:33:17 +0900

    xxx 패키지에서 추가한 테스트 코드 파일입니다.

---
+++ Team_Bongousse/SGCP/src/test/xxx/Problem24.java
@@ -0,0 +1,13 @@
+package test.xxx;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.HashSet;
+ 
+public class Problem24 {	
+	public static final int MAX_KEY_SIZE = 10;
+	public static void main(String args[]){		
+		System.out.println("Hello World");
+	}
+}
