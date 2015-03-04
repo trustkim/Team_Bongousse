d4c311bfbe38fbce8e7ebde9c3d7c65b3821c670
mid-eum <mid-eum@TK-Pavilion>
2015-03-04 15:24:16 +0900

    trustkim.Problem27.java 파일 읽기 부분까지 완성. 파일 읽을 때 char형 [6][5]배열인 grid_A,B로 읽었음

--- Team_Bongousse/SGCP/src/test/trustkim/Problem27.java
+++ Team_Bongousse/SGCP/src/test/trustkim/Problem27.java
@@ -11,10 +11,39 @@ public class Problem27 {
 		try{
 			Scanner input = new Scanner(new File("input27.txt"));
 			for(int T = input.nextInt(); T > 0; T--){			
+				int k = input.nextInt(); input.nextLine();
+				//System.out.println(k);
+				char[][] grid_A = new char[6][5];
+				for(int i=0;i<6;i++) {
+					String temp = input.nextLine();
+					for(int j=0;j<5;j++)
+						grid_A[i][j] = temp.charAt(j);
+				}	// read grid_A
+				//Print_grid(grid_A);
+				char[][] grid_B = new char[6][5];
+				for(int i=0;i<6;i++) {
+					String temp = input.nextLine();
+					for(int j=0;j<5;j++)
+						grid_B[i][j] = temp.charAt(j);
+				}	// read grid_B
+				//Print_grid(grid_B);
+				// file read complete
+				
+				// merge grids or sorting each grids
+				
+				// get k passwords
 				
 			}
 			input.close();
 		}catch(FileNotFoundException e){System.out.println("file not found..");}
 		System.out.println("Elapsed: "+(((long)System.currentTimeMillis())-start)/1000.0);
+	}
+	
+	public static void Print_grid(char[][] grid){
+		for(int i=0;i<6;i++)
+			for(int j=0;j<5;j++){
+				System.out.print(grid[i][j]);
+				if(j==4) System.out.println();
+			}
 	}
 }
