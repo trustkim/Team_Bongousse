cf966dac2263d4f96dcea7cbb4cb5284a20d131c
mid-eum <mid-eum@TK-Pavilion>
2015-03-04 15:51:02 +0900

    trustkim.Problem27.java merge 메소드 추가.
    merge 함수는 두 그리드의 각 wheel(26개 알파벳 대문자가 있는 열에서 6글자를 택한 것으로 그리드의 열벡터?)
    을 검사하여 동일하게 등장하는 문자만 따로 추려 새로운 그리드를 반환한다.
    
    반환하는 자료형도 그리드의 자료형과 동일한 char[6][5]로 반환한다.
    
    각 wheel에서 동일한 문자가 등장했는지 검출은
    A그리드의 한 wheel을 먼저 읽으면서 해쉬셋(6개 공간이면 충분)에 채우고
    대응되는 B그리드의 한 wheel을 읽으면서 해쉬셋에 동일한 문자가 있는지 검사.
    있다면 새로운 그리드에 추가한다.
    
    반환하는 그리드를 반환하기 전에 미리 각 wheel별로 정렬해주는 것도 좋을 것 같다.
    
    한 번더 이 함수가 정확히 동작하는지 확인이 필요하다.

--- Team_Bongousse/SGCP/src/test/trustkim/Problem27.java
+++ Team_Bongousse/SGCP/src/test/trustkim/Problem27.java
@@ -2,6 +2,7 @@ package test.trustkim;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Problem27 {
@@ -30,6 +31,8 @@ public class Problem27 {
 				// file read complete
 				
 				// merge grids or sorting each grids
+				char[][] grid = merge(grid_A, grid_B);
+				Print_grid(grid); System.out.println();
 				
 				// get k passwords
 				
@@ -37,6 +40,25 @@ public class Problem27 {
 			input.close();
 		}catch(FileNotFoundException e){System.out.println("file not found..");}
 		System.out.println("Elapsed: "+(((long)System.currentTimeMillis())-start)/1000.0);
+	}
+	public static char[][] merge(char[][] A, char[][] B){
+		char[][] C = new char[6][5];
+		
+		HashSet<Character> hs = new HashSet<Character>(6);	// hash set for counting arguments.
+		for(int i=0;i<5;i++){
+			int k = 0;
+			
+			for(int j=0;j<6;j++){
+				hs.add(A[j][i]);
+			}
+			for(int j=0;j<6;j++){
+				if(hs.contains(B[j][i])){
+					C[k++][i] = B[j][i];
+				}
+			}
+			hs.clear();
+		}
+		return C;
 	}
 	
 	public static void Print_grid(char[][] grid){
