e09c6048ad744b7fdc8c2199fa4000e53aa9097e
mid-eum <mid-eum@TK-Pavilion>
2015-03-12 21:09:49 +0900

    30번 문제 풀이 시도 시작 solve는 메이즈와 같은 형태. 하지만 다른점은 출구에 도착하면 해당 경로의 걸린 시간을 출력해줌 최단 시간은 아님.
    아직까지 불 장애물은 고정이다.
    불 붙는 것을 위해 burn 함수를 만들어 둠
    시간복잡도가 불안하긴 하지만 나중에
    각 불들의 좌표를 갖고 있다가 solve 실행 도중 t가 k의 배수가 되면
    모든 불 좌표들의 셀에 대해서만 burn을 호출 해줌

---
+++ Team_Bongousse/SGCP/src/test/trustkim/Problem30.java
@@ -0,0 +1,76 @@
+package test.trustkim;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Problem30 {
+	private static int [][] grid;
+	private static int N;
+	private static int K;
+	private static int time;
+	private static final int PATHWAY = 0;
+	private static final int FIRE = -1;
+	private static final int BLOCKED = -2;
+	private static final int PATH = 1;
+	private static Queue<int[]> queue;
+	
+	public static void main(String [] args) {
+		long start = System.currentTimeMillis();
+		try {
+			Scanner input = new Scanner(new File("input30.txt"));
+			for(int T=input.nextInt();T>0;T--) {
+
+				N = input.nextInt(); K = input.nextInt();
+				grid = new int[N][N];
+				for(int i=0;i<N;i++)
+					for(int j=0;j<N;j++){
+						grid[i][j]=input.nextInt()==0?0:-1;
+					}// file read complete
+				
+				// data structure setting
+				Print(grid);
+				
+				// solve
+				time=0;
+				System.out.println(solve(0,0,time)+" "+time);
+			}
+		}catch (FileNotFoundException e) { e.printStackTrace();}
+		System.out.println("Elapsed: "+(((long)System.currentTimeMillis())-start)/1000.0);
+	}
+	public static boolean solve(int x0, int y0, int t) {
+		if(x0<0||y0<0||x0>N-1||y0>N-1) return false;
+		else if(grid[x0][y0]!=PATHWAY) return false;
+		else if(x0==N-1&&y0==N-1) {
+			grid[x0][y0] = PATH; time=t;
+			return true;
+		}
+		else {
+			grid[x0][y0] = PATH;
+			if(solve(x0-1,y0,t+1) || solve(x0,y0+1,t+1)
+				|| solve(x0+1,y0,t+1) || solve(x0,y0-1,t+1)) return true; 
+			grid[x0][y0] = BLOCKED;
+			return false;
+		}
+	}
+
+	public static int[][] burn(int[][] grid, int x, int y) {
+		if(x-1>=0&&y>=0&&x-1<N&&y<N)
+			grid[x-1][y] = FIRE;
+		if(x>=0&&y+1>=0&&x<N&&y+1<N)
+			grid[x][y+1] = FIRE;
+		if(x+1>=0&&y>=0&&x+1<N&&y<N)
+			grid[x+1][y] = FIRE;
+		if(x>=0&&y-1>=0&&x<N&&y-1<N)
+			grid[x][y-1] = FIRE;
+		return grid;
+	}
+	public static void Print(int [][] grid) {
+		for(int i=0;i<grid.length;i++){
+			for(int j=0;j<grid[i].length;j++)
+				System.out.print((grid[i][j]<0?1:0)+" ");
+			System.out.println();
+		}
+	}
+}
