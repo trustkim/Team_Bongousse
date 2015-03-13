8f484b7a67ae646b9294efdec9ac779f3cb5c916
trustkim <trustkim90@gmail.com>
2015-03-14 01:26:40 +0900

    30번 최종 제출본 입니다. 필요 없는 코드 싹 지우고 주석 좀 달고 골자는 BFS입니다 큐 사용한 걸 눈여겨 보세요.

--- Team_Bongousse/SGCP/src/Problem30.java
+++ Team_Bongousse/SGCP/src/Problem30.java
@@ -5,83 +5,80 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Problem30 {
-	private static int [][] grid;
-	private static int [][] fireMaze;
 	private static int N;
 	private static int K;
+	private static int [][] grid;
+	private static int [][] fireBurn;
+	private static Queue<int[]> queue;
+
 	private static final int MAX = 2000;
 	private static final int PATHWAY = 0;
 	private static final int FIRE = 1;
-	private static final int BLOCKED = 2;
-	private static final int PATH = 3;
-	private static Queue<int[]> fireQ;
-	private static Queue<int[]> queue;
-	
+	private static final int PATH = 2;
+
 	public static void main(String [] args) {
 		try {
 			Scanner input = new Scanner(new File("input30.txt"));
 			for(int T=input.nextInt();T>0;T--) {
 				N = input.nextInt(); K = input.nextInt();
 				grid = new int[N][N]; 
-				fireMaze = new int[N][N];
-				fireQ = new LinkedList<int[]>();
+				fireBurn = new int[N][N];
+				queue = new LinkedList<int[]>();
 				for(int i=0;i<N;i++)
 					for(int j=0;j<N;j++){
 						grid[i][j]=input.nextInt();
-						fireMaze[i][j]=grid[i][j]==1?0:MAX;
-						if(fireMaze[i][j]==0) fireQ.offer(new int[]{i,j,0});
+						fireBurn[i][j]=(grid[i][j]==FIRE?0:MAX);
+						if(fireBurn[i][j]==0) queue.offer(new int[]{i,j,0});
 					}// file read complete
 
-				// data structure setting
-				boolean[][] isChecked = new boolean[N][N];
-				while(!fireQ.isEmpty()){
-					int[] cell = fireQ.poll();
-					int x=cell[0];
-					int y=cell[1];
-					int t=cell[2];
-					
-					if(isChecked[x][y]) ;
-					else {
-						isChecked[x][y] = true;
-						fireMaze[x][y] = Math.min(fireMaze[x][y], t);
-						if(x-1>=0&&y>=0&&x-1<N&&y<N&&fireMaze[x-1][y]==MAX)
-							fireQ.add(new int[]{x-1,y,K+t});
-						if(x>=0&&y+1>=0&&x<N&&y+1<N&&fireMaze[x][y+1]==MAX)
-							fireQ.add(new int[]{x,y+1,K+t});
-						if(x+1>=0&&y>=0&&x+1<N&&y<N&&fireMaze[x+1][y]==MAX)
-							fireQ.add(new int[]{x+1,y,K+t});
-						if(x>=0&&y-1>=0&&x<N&&y-1<N&&fireMaze[x][y-1]==MAX)
-							fireQ.add(new int[]{x,y-1,K+t});	
-					}
-				}
-				
-				// solve
-				queue = new LinkedList<int[]>();
+				fireBFS();
 				BFS(0,0);
 			}
 		}catch (FileNotFoundException e) { e.printStackTrace();}
 	}
+	public static void fireBFS() {
+		boolean[][] isChecked = new boolean[N][N];
+		while(!queue.isEmpty()){
+			int[] cell = queue.poll();
+			int x=cell[0];
+			int y=cell[1];
+			int t=cell[2];
+			
+			if(isChecked[x][y]) ;
+			else {
+				isChecked[x][y] = true;
+				fireBurn[x][y] = Math.min(fireBurn[x][y], t);
+				if(x-1>=0&&y>=0&&x-1<N&&y<N&&fireBurn[x-1][y]==MAX)	// 위
+					queue.offer(new int[]{x-1,y,K+t});
+				if(x>=0&&y+1>=0&&x<N&&y+1<N&&fireBurn[x][y+1]==MAX)	// 오른
+					queue.offer(new int[]{x,y+1,K+t});
+				if(x+1>=0&&y>=0&&x+1<N&&y<N&&fireBurn[x+1][y]==MAX)	// 아래
+					queue.offer(new int[]{x+1,y,K+t});
+				if(x>=0&&y-1>=0&&x<N&&y-1<N&&fireBurn[x][y-1]==MAX)	// 왼
+					queue.offer(new int[]{x,y-1,K+t});	
+			}
+		}
+	}
 	public static void BFS(int x0, int y0){
 		queue.offer(new int[]{x0,y0,0});
 		while(!queue.isEmpty()){
-			int[] vertex = queue.poll();
-			int x=vertex[0];
-			int y=vertex[1];
-			int t=vertex[2];
+			int[] cell = queue.poll();
+			int x=cell[0];
+			int y=cell[1];
+			int t=cell[2];
 			
-			if(grid[x][y]!=PATHWAY||fireMaze[x][y]<t+1) ;
+			if(grid[x][y]!=PATHWAY||fireBurn[x][y]<=t) ;	// grid에서 길이 아니거나, fireBurn에서 이미 불 붙은 경우
 			else if(x==N-1&&y==N-1) {System.out.println(t); return;}
 			else {
 				grid[x][y] = PATH;
-				if(x-1>=0&&y>=0&&x-1<N&&y<N&&grid[x-1][y]==PATHWAY)
+				if(x-1>=0&&y>=0&&x-1<N&&y<N&&grid[x-1][y]==PATHWAY)	// 위
 					queue.offer(new int[]{x-1,y,t+1});
-				if(x>=0&&y+1>=0&&x<N&&y+1<N&&grid[x][y+1]==PATHWAY)
+				if(x>=0&&y+1>=0&&x<N&&y+1<N&&grid[x][y+1]==PATHWAY)	// 오른
 					queue.offer(new int[]{x,y+1,t+1});
-				if(x+1>=0&&y>=0&&x+1<N&&y<N&&grid[x+1][y]==PATHWAY)
+				if(x+1>=0&&y>=0&&x+1<N&&y<N&&grid[x+1][y]==PATHWAY)	// 아래
 					queue.offer(new int[]{x+1,y,t+1});
-				if(x>=0&&y-1>=0&&x<N&&y-1<N&&grid[x][y-1]==PATHWAY)
+				if(x>=0&&y-1>=0&&x<N&&y-1<N&&grid[x][y-1]==PATHWAY)	// 왼
 					queue.offer(new int[]{x,y-1,t+1});
-				grid[x][y] = BLOCKED;
 			}
 		}
 		System.out.println(-1);
