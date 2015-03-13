cbd773157f76dc709ef76aad63a4c566aa87ba4b
mid-eum <mid-eum@TK-Pavilion>
2015-03-13 15:59:41 +0900

    30번 사전에 모든 셀에 불이 언제 붙을 지 계산하는 코드를 추가함 충분한 검증을 거친후에 필요하다면 이후 함수로 만들어 둘 예정
    임시로 grid와 별개로 maze 배열을 만들어 씀
    어떤 셀도 MAX=2000초 안에 불 붙을 수 없으므로 불이 붙지 않은 지점은 2000으로 잡고
    추가한 코드를 지나면 언제 불이 붙는지 값을 저장하게 된다. MAX의 사용이 적절한지 여부의 검토가 필요.
    
    isChecked 배열이 필요한가? 없앨 수 없는가?
    
    간단한 테스트 결과를 돌려봐도 이전의 BFS함수는 잘못된 것을 알 수 있다.
    이후 많은 점이 변경될 예정. 함수 이름도 갑자기 바뀌더라도 당황하지 않길 바란다.

--- Team_Bongousse/SGCP/src/test/trustkim/Problem30.java
+++ Team_Bongousse/SGCP/src/test/trustkim/Problem30.java
@@ -11,11 +11,11 @@ public class Problem30 {
 	private static int [][] grid;
 	private static int N;
 	private static int K;
-	private static int time;
+	private static final int MAX = 2000;
 	private static final int PATHWAY = 0;
-	private static final int FIRE = -1;
-	private static final int BLOCKED = -2;
-	private static final int PATH = 1;
+	private static final int FIRE = 1;
+	private static final int PATH = 2;
+	private static Queue<int[]> fireQ;
 	private static Queue<int[]> queue;
 	
 	public static void main(String [] args) {
@@ -25,37 +25,48 @@ public class Problem30 {
 			for(int T=input.nextInt();T>0;T--) {
 
 				N = input.nextInt(); K = input.nextInt();
-				grid = new int[N][N];
+				grid = new int[N][N]; 
+				int[][] maze = new int[N][N];
+				fireQ = new LinkedList<int[]>();
 				for(int i=0;i<N;i++)
 					for(int j=0;j<N;j++){
-						grid[i][j]=input.nextInt()==0?0:-1;
+						grid[i][j]=input.nextInt();
+						maze[i][j]=grid[i][j]==1?0:MAX;
+						if(maze[i][j]==0) fireQ.offer(new int[]{i,j,0});
 					}// file read complete
 				
 				// data structure setting
-				//Print(grid);
-				
+				boolean[][] isChecked = new boolean[N][N];
+				while(!fireQ.isEmpty()){
+					int[] cell = fireQ.poll();
+					int x=cell[0];
+					int y=cell[1];
+					int t=cell[2];
+					
+					if(isChecked[x][y]) ;
+					else {
+						isChecked[x][y] = true;
+						maze[x][y] = Math.min(maze[x][y], t);
+						if(x-1>=0&&y>=0&&x-1<N&&y<N&&maze[x-1][y]==MAX)
+							fireQ.add(new int[]{x-1,y,K+t});
+						if(x>=0&&y+1>=0&&x<N&&y+1<N&&maze[x][y+1]==MAX)
+							fireQ.add(new int[]{x,y+1,K+t});
+						if(x+1>=0&&y>=0&&x+1<N&&y<N&&maze[x+1][y]==MAX)
+							fireQ.add(new int[]{x+1,y,K+t});
+						if(x>=0&&y-1>=0&&x<N&&y-1<N&&maze[x][y-1]==MAX)
+							fireQ.add(new int[]{x,y-1,K+t});	
+					}
+				}
+				System.out.println("THIS IS GRID");
+				Print(grid);
+				System.out.println("THIS IS MAZE");
+				Print(maze);
 				// solve
 				queue = new LinkedList<int[]>();
-				//System.out.println(solve(0,0,time)+" "+time);
 				BFS(0,0);
 			}
 		}catch (FileNotFoundException e) { e.printStackTrace();}
 		System.out.println("Elapsed: "+(((long)System.currentTimeMillis())-start)/1000.0);
-	}
-	public static boolean solve(int x0, int y0, int t) {
-		if(x0<0||y0<0||x0>N-1||y0>N-1) return false;
-		else if(grid[x0][y0]!=PATHWAY) return false;
-		else if(x0==N-1&&y0==N-1) {
-			grid[x0][y0] = PATH; time=t;
-			return true;
-		}
-		else {
-			grid[x0][y0] = PATH;
-			if(solve(x0-1,y0,t+1) || solve(x0,y0+1,t+1)
-				|| solve(x0+1,y0,t+1) || solve(x0,y0-1,t+1)) return true; 
-			grid[x0][y0] = BLOCKED;
-			return false;
-		}
 	}
 
 	public static int[][] burn(int[][] grid, int x, int y) {
@@ -80,22 +91,16 @@ public class Problem30 {
 
 			if(x==N-1&&y==N-1) {System.out.println(t); return;}
 			else {
-				
 				grid[x][y] = PATH;
-				if(x-1>=0&&y>=0&&x-1<N&&y<N&&grid[x-1][y]==0)
+				if(x-1>=0&&y>=0&&x-1<N&&y<N&&grid[x-1][y]==PATHWAY)
 					queue.add(new int[]{x-1,y,t+1});
-				if(x>=0&&y+1>=0&&x<N&&y+1<N&&grid[x][y+1]==0)
+				if(x>=0&&y+1>=0&&x<N&&y+1<N&&grid[x][y+1]==PATHWAY)
 					queue.add(new int[]{x,y+1,t+1});
-				if(x+1>=0&&y>=0&&x+1<N&&y<N&&grid[x+1][y]==0)
+				if(x+1>=0&&y>=0&&x+1<N&&y<N&&grid[x+1][y]==PATHWAY)
 					queue.add(new int[]{x+1,y,t+1});
-				if(x>=0&&y-1>=0&&x<N&&y-1<N&&grid[x][y-1]==0)
+				if(x>=0&&y-1>=0&&x<N&&y-1<N&&grid[x][y-1]==PATHWAY)
 					queue.add(new int[]{x,y-1,t+1});
-				
-				if(t+1%K==0) {
-					for(int i=0;i<N;i++)
-						for(int j=0;j<N;j++)
-							if(grid[i][j]==FIRE) burn(grid,i,j);
-				}
+
 			}
 		}
 		System.out.println(-1);
@@ -103,7 +108,7 @@ public class Problem30 {
 	public static void Print(int [][] grid) {
 		for(int i=0;i<grid.length;i++){
 			for(int j=0;j<grid[i].length;j++)
-				System.out.print((grid[i][j]<0?1:0)+" ");
+				System.out.print(grid[i][j]+" ");
 			System.out.println();
 		}
 	}
