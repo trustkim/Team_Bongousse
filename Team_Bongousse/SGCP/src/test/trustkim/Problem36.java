61f7916383e669177ba3cac64ec0a8296dc2434e
trustkim <trustkim90@gmail.com>
2015-04-05 17:04:57 +0900

    36번 신체발부수지부모팀 카피, 교수님 입력 테스트파일 추가

--- Team_Bongousse/SGCP/src/test/trustkim/Problem36.java
+++ Team_Bongousse/SGCP/src/test/trustkim/Problem36.java
@@ -2,58 +2,37 @@ package test.trustkim;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class Problem36 {
-	static class Node {
-		char head, tail;
-		int index;
-		Node next;
-		Node(char h, char t, int i) { head=h; tail=t; index=i; next=null;}
-		Node(Node A) {head=A.head; tail=A.tail; index=A.index; next=null;}
-		public static Comparator<Node> headComparator = new Comparator<Node>() {
-			public int compare(Node A, Node B) {
-				return (A.head - B.head);
-			}
-		};
-		public static Comparator<Node> tailComparator = new Comparator<Node>() {
-			public int compare(Node A, Node B) {
-				return (A.tail - B.tail);
-			}
-		};
-	}
 	private static int N;
-	private static Node[] data;
-	private static Node[] heads;
-	private static Node[] tails;
-	private static final int HEAD = 0;
-	private static final int TAIL = 1;
-	private static boolean[] isChecked;
+	private static int[] indegree;				// 알파벳 문자별로 입력 차수를 세기 위한 배열
+	private static int[] outdegree;				// 알파벳 문자별 출력 차수를 세기 위한 배열
+	private static int[] where_is_destination;
+	// 각 알파벳 문자를 정점으로 그래프 모델링 할 때, 해당 문자에서 출발하여 최종 도달할 수 있는 문자 저장(최초 세팅을 어떻게 하느냐에 따라 결정됨)
+	
 	public static void main(String[] args){
 		try {
 			Scanner sc = new Scanner(new File("input36.txt"));
 			String temp;
 			for(int T = sc.nextInt();T>0;T--) {
 				N = sc.nextInt();
-				data = new Node[N];
-				heads = new Node[N];
-				tails = new Node[N];
-				isChecked = new boolean[N];
+				indegree = new int[26];				// 0으로 초기화
+				outdegree = new int[26];			// 0으로 초기화
+				where_is_destination = new int[26];	// 모든 알파벳의 도착지를 자신으로 초기화
+				for(int i=0;i<26;i++) where_is_destination[i] = i;
 				for(int i=0;i<N;i++) {
 					temp = sc.next().trim();
-					data[i] = new Node(temp.charAt(0), temp.charAt(temp.length()-1), i);
-					heads[i] = new Node(data[i]);
-					tails[i] = new Node(data[i]);
-					System.out.println(data[i].head +", "+data[i].tail);
-					isChecked[i] = false;
+					int start = temp.charAt(0)-'a';
+					int end = temp.charAt(temp.length()-1)-'a';
+					indegree[start]++;	// start로 시작하는 단어이면 입력 차수를 카운트
+					outdegree[end]++;	// end로 끝나는 단어이면 출력 차수를 카운트
+					int sdest = findDest(start);
+					int edest = findDest(end);
+					if(sdest!=edest)
+						where_is_destination[sdest] = edest;
 				}// file read complete
 				
-				Arrays.sort(heads, 0, N, Node.headComparator);
-				Arrays.sort(tails, 0, N, Node.tailComparator);
-				for(int i=0;i<N;i++)
-					System.out.println(heads[i].head+" <-? "+tails[i].tail);
 				if(solve()){
 					System.out.println("YES");
 				}else System.out.println("NO");
@@ -62,33 +41,26 @@ public class Problem36 {
 			sc.close();
 		} catch(FileNotFoundException e){ System.out.println("file not found..."); }
 	}
-	
-	private static boolean solve() {
-		for(int i=0;i<N;i++)							// TAIL 배열 인덱스
-			for(int j=0;j<N;j++) {						// HEAD 배열 인덱스
-				if(!isChecked[j]) {
-					if(N!=1 && heads[j].index==tails[i].index) {		// 자기 자신은 가리킬 수 없다
-														// 넘어감
-					}else if(heads[j].head == tails[i].tail) {
-						isChecked[j] = true;
-						data[tails[i].index].next = data[heads[j].index];
-						System.out.println(heads[j].index+" <-! "+tails[i].index);
-						j=N; break;
-					}
-					else if(heads[j].head > tails[i].tail) return false;
-				}
-			}		
-		return test_one_cycle();
-	}
-	private static boolean test_one_cycle() {
-		Node Cur = data[0];
-		int cnt = 0;
-		while(Cur.next!=null) {
-			Cur = Cur.next;
-			cnt++;
-			if(Cur.index==0) break;
+	/*
+	 * 신체발부수지부모팀의 그것과 동일한 findDest는 다음 링크를 참조하였다.
+	 * 참조 링크: http://egloos.zum.com/mgkim/v/1007300
+	 */
+	private static int findDest(int v) {
+		if(where_is_destination[v]==v) {
+			return v;
+		}else {
+			where_is_destination[v] = findDest(where_is_destination[v]);
+			return where_is_destination[v];
 		}
-		if(cnt==N) return true;
-		else return false;
+	}
+	private static boolean solve() {
+		for(int i=0,sample_destination=0;i<26;i++) {
+			if(indegree[i]>0||outdegree[i]>0) {	// 차수가 0이상이면 한 번이라도 등장한 문자임
+				if(sample_destination==0) sample_destination = findDest(i);
+				if(indegree[i]!=outdegree[i]||findDest(i)!=sample_destination) return false;
+				// 한 붓 그리기 가능 한지, 어떤 알파벳으로도 동일한 종착지에 도달하는지 검사
+			}
+		}		
+		return true;
 	}
 }
