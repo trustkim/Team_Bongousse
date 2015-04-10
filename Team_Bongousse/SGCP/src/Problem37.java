b10de9e1d65b33e9090b0252587db6c1a9c9366a
mid-eum <mid-eum@TK-Pavilion>
2015-04-10 15:40:15 +0900

    37번 문제 제출본 커밋. 시간복잡도는 O(N*N)임 단 문제에는 드러나 있지 않지만 입력파일 형식이 이진 트리로만 주어진다는 가정하에 풀었다. 따라서 별도로 동일 노드에 자식이 3이상 형성 되는 경우는 따로 체크하지 않았다.
    힙소트 할 때 했던 배열 구조에 주어진 노드 정보로 트리를 만듦.
    한 노드를 읽고 이 노드의 부모 값을 현재 만들어진 트리 배열에서 동일한 값을 찾고,O(N)
    이렇게 N개의 노드로 트리를 만드니 트리를 형성하는데 O(N*N)이 걸린다.
    
    중간에 미리 설정해둔 트리 배열의 인덱스를 초과하게 형성 되는 트리는
    완전이진트리의 높이 조건에 위배 되므로 promissing 플래그를 false로 해둠.
    
    이렇게 트리를 형성해서 트리 배열에 하나라도 빈칸이 있다면 완전이진 트리가 아님...
    
    앗! 커밋 메세지를 적다가 굳이 트리 형성 후에 이 조건을 검사하지 않아도 된다는 것을 깨달음.
    그냥 promissing 만으로 검사하게 하여 제출해봄.

---
+++ Team_Bongousse/SGCP/src/Problem37.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Problem37 {
+	private static int N;
+	private static int[] tree;
+	public static void main(String[] args) {
+		try {
+			Scanner sc = new Scanner(new File("input37.txt"));
+			boolean promissing = true;	// 파일 읽는 중(트리를 형성하는 중) 완전 이진 트리의 높이 조건을 검사하여 한 케이스를 걸러 낼 수 있다.
+			for(int T=sc.nextInt();T>0;T--) {
+				N=sc.nextInt();			// 검사할 트리 노드 개수
+				tree = new int[N];	// 트리를 저장할 배열을 만듦
+				tree[0] = 1;				// root는 항상 1로
+				int p,c,f;					// p: 부모, c: p의 자식, f: 왼쪽/오른쪽 자식 플래그
+				for(int i=1;i<N;i++) {		// N-1 loop
+					p=sc.nextInt(); c = sc.nextInt(); f = sc.nextInt();
+					int childIndex = 2*getIndex(p)+1+f;
+					if(childIndex<N)
+						tree[childIndex] = c;
+					else {
+						promissing = false;	// 현재 케이스 포문 중단해버리면 파일읽던 포인터 맞춰 줘야 함...
+					}
+				}// file read complete
+				
+				if(promissing&&solve()) System.out.println("Yes");
+				else System.out.println("No");
+			}
+			sc.close();
+		}catch(FileNotFoundException e) { System.out.println("file not found...");}
+	}
+	private static int getIndex(int p) {	// 트리 배열에서 동일한 원소 값을 찾아 그 인덱스를 반환
+		for(int i=0;i<N;i++){
+			if(tree[i]==p) return i;
+		}
+		return -1;
+	}	// O(N*N) 인가? 이렇게 하는거랑 연결리스트로 트리 만들어서 각 노드 검사하는 거랑 어느게 더 낮지?
+	private static boolean solve() {	// 만들어진 트리를 검사하여 중간에 빈 곳이 있는지 검사		
+		for(int i=0;i<N;i++) {
+			if(tree[i]<1) return false; 
+		}
+		return true;
+	}	// O(N)
+}
